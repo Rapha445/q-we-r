ff9f1c1a3edcf75eabb35439974ec9d38d66f0f4
Rapha <raphaelle.delmas@lilo.org>
2022-04-20 19:08:51 +0900

    info update

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -107,6 +107,102 @@ $('.slider-index__slider').each(function() {
   });
 
 
+
+$('.slider-event__slider').each(function() {
+  var $this = $(this);
+  var $group = $this.find('.slider-event__slide_group');
+  var $slides = $this.find('.slider-event__slide');
+  var bulletArray = [];
+  var currentIndex = 0;
+  var timeout;
+  
+  function move(newIndex) {
+    var animateLeft, slideLeft;
+    
+    advance();
+    
+    if ($group.is(':animated') || currentIndex === newIndex) {
+      return;
+    }
+    
+    bulletArray[currentIndex].removeClass('active');
+    bulletArray[newIndex].addClass('active');
+    
+    if (newIndex > currentIndex) {
+      slideLeft = '100%';
+      animateLeft = '-100%';
+    } else {
+      slideLeft = '-100%';
+      animateLeft = '100%';
+    }
+    
+    $slides.eq(newIndex).css({
+      display: 'block',
+      left: slideLeft
+    });
+    $group.animate({
+      left: animateLeft
+    }, function() {
+      $slides.eq(currentIndex).css({
+        display: 'none'
+      });
+      $slides.eq(newIndex).css({
+        left: 0
+      });
+      $group.css({
+        left: 0
+      });
+      currentIndex = newIndex;
+    });
+  }
+  
+  function advance() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      if (currentIndex < ($slides.length - 1)) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
+    }, 4000);
+  }
+  
+  $('.slider-event__next_btn').on('click', function() {
+    if (currentIndex < ($slides.length - 1)) {
+      move(currentIndex + 1);
+    } else {
+      move(0);
+    }
+  });
+  
+  $('.slider-event__previous_btn').on('click', function() {
+    if (currentIndex !== 0) {
+      move(currentIndex - 1);
+    } else {
+      move(3);
+    }
+  });
+  
+  $.each($slides, function(index) {
+    var $button = $('<a class="slider-event__slide_btn">&bull;</a>');
+    
+    if (index === currentIndex) {
+      $button.addClass('active');
+    }
+    $button.on('click', function() {
+      move(index);
+    }).appendTo('.slider-event__slide_buttons');
+    bulletArray.push($button);
+  });
+  
+  advance();
+});
+
+
 if (module.hot) {
-    module.hot.accept()
+  module.hot.accept()
+};
+
+if (module.hot) {
+  module.hot.accept()
 };
