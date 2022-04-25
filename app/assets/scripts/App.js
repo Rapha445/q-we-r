519f7da0b98c024cffdba33c0ed181a033475e74
Rapha <raphaelle.delmas@lilo.org>
2022-04-25 16:29:22 +0900

    return to design

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,18 +4,6 @@ import HamburgerMenu from './modules/HamburgerMenu';
 
 new HamburgerMenu();
 
-let modal;
-window.addEventListener("DOMContentLoaded", e => {
-    if (typeof modal == "undefined") {
-        import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
-            modal = new x.default();
-            setTimeout(() => modal.openTheModal(), 20);
-        }).catch(() => console.log("error"));
-    } else {
-        modal.openTheModal()
-    }
-})
-
 if (module.hot) {
     module.hot.accept()
 };
